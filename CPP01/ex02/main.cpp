54ae4b3d53c27705ed92699ba2d104034a907597
zzzeelloo <102261049+zzzeelloo@users.noreply.github.com>
2026-01-06 15:15:15 +0000

    ex02

---
+++ CPP01/ex02/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main()
+{
+    std::string brain = "HI THIS IS BRAIN";
+    std::string *stringPTR = &brain;
+    std::string &stringREF = brain;
+
+    std::cout << "Memory adresses : " << "-brain ->"  << &brain << std::endl;
+    std::cout << "-stringPTR ->" << &stringPTR << std::endl;
+    std::cout << "-stringREF ->" << &stringREF <<  std::endl;
+
+    std::cout << "Value of brain = " << brain << std::endl;
+    std::cout << "value pointed by stringPTR = " << *stringPTR << std::endl;
+    std::cout << "value pointed by stringREF = " << stringREF << std::endl;
+
+    return(0);
+
+}
